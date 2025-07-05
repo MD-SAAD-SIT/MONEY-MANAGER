e26514895436a0b07e8cceaedc97d7c661758f56
MD-SAAD-SIT <mdsaad7019i@gmail.com>
2025-07-05 21:10:59 +0530

    res.send() in controllers

--- controllers/info.js
+++ controllers/info.js
@@ -3,18 +3,23 @@ const transaction = require('../models/transaction');
 
 const getBalance = async (req, res) => {
     console.log('getting balance');
+    res.send("getting balance");
 }
 
 const editBalance = async (req, res) => {
     console.log('editing balance');
+    res.send("editing balance");
 }
 
 const updations = async (req, res) => {
-    console.log('doing processes')
+    console.log('doing processes');
+    res.send("processing transactions");
+
 }
 
 const fullfill = async (req, res) => {
-    console.log('money fullfilled')
+    console.log('money fullfilled');
+    res.send("fullfilled");
 }
 
 module.exports = { getBalance, editBalance, updations, fullfill };
