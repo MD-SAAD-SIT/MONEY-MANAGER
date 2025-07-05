a55c64250e7d2b8e8a9453f22a9bb0ee5291e94a
MD-SAAD-SIT <mdsaad7019i@gmail.com>
2025-07-05 20:28:59 +0530

    minimized controlers

--- controllers/info.js
+++ controllers/info.js
@@ -9,28 +9,12 @@ const editBalance = async (req, res) => {
     console.log('editing balance');
 }
 
-const addMoney = async (req, res) => {
-    console.log('adding money');
+const updations = async (req, res) => {
+    console.log('doing processes')
 }
 
-const withdrawMoney = async (req, res) => {
-  console.log("withdrawing money");
-};
-
-const lendMoney = async (req, res) => {
-    console.log('lending money');
+const fullfill = async (req, res) => {
+    console.log('money fullfilled')
 }
 
-const fullfillLendMoney = async (req, res) => {
-    console.log('lend money fullfilled')
-}
-
-const borrowMoney = async (req, res) => {
-    console.log('borrowing money');
-}
-
-const fullfillBorrowMoney = async (req, res) => {
-  console.log("borrow money fullfilled");
-}
-
-module.exports = { getBalance, editBalance,addMoney, withdrawMoney, lendMoney, fullfillLendMoney, borrowMoney, fullfillBorrowMoney };
+module.exports = { getBalance, editBalance, updations, fullfill };
