8808a8ae302378e3a6ee095797a3bcf76ae62cdf
MD-SAAD-SIT <mdsaad7019i@gmail.com>
2025-07-05 20:11:27 +0530

    add fullfillment option

--- models/transaction.js
+++ models/transaction.js
@@ -3,6 +3,7 @@ const trackSchema = new mongoose.Schema({
   action: { type: String, enum: ["ADD", "WITHDRAW", "LEND", "BORROW"] },
   amount: { type: Number },
   transactionMode: { type: String, enum: ["BANK", "CASH"] },
+  fullfilled : { type: String, enum: ["YES", "NO"]},
   message: { type: String, default: "no message" },
   date: { type: String, default: () => new Date().toISOString().slice(0, 10) },
   time: { type: String, default: () => new Date().toTimeString().slice(0, 5) },
