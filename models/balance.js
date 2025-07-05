27137b8b76dad3cf7e088fa8410516f85808f3c1
MD-SAAD-SIT <mdsaad7019i@gmail.com>
2025-07-05 22:30:30 +0530

    added default for model balance

--- models/balance.js
+++ models/balance.js
@@ -3,14 +3,17 @@ const mongoose = require("mongoose");
 const balanceSchema = new mongoose.Schema({
   balance: {
     type: Number,
+    default: 0,
     required: true,
   },
   acount: {
     type: Number,
+    default: 0,
     required: true,
   },
   cash: {
     type: Number,
+    default: 0,
     required: true,
   },
 });
