a9ca25b48e02fddce1ae070194d480ce8f40c209
MD-SAAD-SIT <mdsaad7019i@gmail.com>
2025-07-09 17:41:08 +0530

    connected index.html to backend

---
+++ public/index.js
@@ -0,0 +1,25 @@
+const balance = document.querySelector('.balance');
+const bank = document.querySelector('.bank');
+const cash = document.querySelector('.cash');
+
+const showBalance = async () => {
+    try {
+        const {
+            data: { bal }
+        } = await axios.get('/api/v1/money');
+        const formatter = new Intl.NumberFormat("en-IN", {
+          maximumFractionDigits: 2,
+        });
+
+        balance.innerHTML = `BALANCE : ₹ ${formatter.format(bal.BALANCE)}`;
+        bank.innerHTML = `BANK : ₹ ${formatter.format(bal.BANK)}`;
+        cash.innerHTML = `CASH : ₹ ${formatter.format(bal.CASH)}`;
+        
+        
+    } catch (error) {
+        balance.innerHTML = `BALANCE : ERROR`;
+        bank.innerHTML = `BANK : ERROR`;
+        cash.innerHTML = `CASH : ERROR`;
+    }
+}
+showBalance();
